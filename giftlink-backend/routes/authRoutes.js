f8a7d1ec9c3de4e5200904ee99bb16619e426442
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-02-15 20:28:36 +0800

    feat: update register

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -19,9 +19,51 @@ const logger = pino();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 
-// ================= REGISTER ROUTE =================
 router.post('/register', async (req, res) => {
-    // Step 2 will be implemented in next task
+    try {
+        // Task 1: Connect to `giftsdb` in MongoDB
+        const db = await connectToDatabase();
+
+        // Task 2: Access MongoDB collection
+        const collection = db.collection("users");
+
+        // Task 3: Check for existing email
+        const existingEmail = await collection.findOne({ email: req.body.email });
+        if (existingEmail) {
+            return res.status(400).json({ error: "Email already exists" });
+        }
+
+        // Hash password
+        const salt = await bcryptjs.genSalt(10);
+        const hash = await bcryptjs.hash(req.body.password, salt);
+        const email = req.body.email;
+
+        // Task 4: Save user details in database
+        const newUser = await collection.insertOne({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+            createdAt: new Date(),
+        });
+
+        // Task 5: Create JWT authentication
+        const payload = {
+            user: {
+                id: newUser.insertedId,
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+
+        logger.info('User registered successfully');
+
+        res.json({ authtoken, email });
+
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+    }
 });
+
 
 module.exports = router;
