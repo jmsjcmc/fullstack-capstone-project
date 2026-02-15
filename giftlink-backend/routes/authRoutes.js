3f61ac639e992552260ecc73069ab511738aa367
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-02-15 20:20:20 +0800

    feat: add authroutes

---
+++ giftlink-backend/routes/authRoutes.js
@@ -0,0 +1,27 @@
+// Step 1 - Task 2: Import necessary packages
+const express = require('express');
+const app = express();
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+const connectToDatabase = require('../models/db');
+const router = express.Router();
+const dotenv = require('dotenv');
+const pino = require('pino');  // Import Pino logger
+
+// Load environment variables
+dotenv.config();
+
+// Step 1 - Task 3: Create a Pino logger instance
+const logger = pino();
+
+// Step 1 - Task 4: Create JWT secret
+const JWT_SECRET = process.env.JWT_SECRET;
+
+
+// ================= REGISTER ROUTE =================
+router.post('/register', async (req, res) => {
+    // Step 2 will be implemented in next task
+});
+
+module.exports = router;
